b9c9ce5caf24c129f2e9c4802bec15a9c3a8facf
Mario Hamann <mariovhamann@gmail.com>
2024-03-01 16:55:57 +0100

    fix export and styles

--- src/activity-graph-element.mjs
+++ src/activity-graph-element.mjs
@@ -1,4 +1,4 @@
-export default function ActivityGraphElement({ html = String.raw, state }) {
+function ActivityGraphElement({ html = String.raw, state }) {
 	const css = String.raw;
 	const { attrs = {} } = state;
 
@@ -33,9 +33,7 @@ export default function ActivityGraphElement({ html = String.raw, state }) {
 	};
 
 	function render() {
-		let innerHtml = html`<style>
-			${getStyle()}
-		</style>`;
+		let innerHtml = html`${getStyle()}`;
 		innerHtml += html`<figure>
 			<table>
 				<tbody>
@@ -241,153 +239,162 @@ export default function ActivityGraphElement({ html = String.raw, state }) {
 	}
 
 	function getStyle() {
-		return css`
-			/* Global */
-			activity-graph {
-				color-scheme: light dark;
-				font-size: 12px;
-				display: block;
-			}
-			activity-graph figure {
-				overflow-x: auto;
-				margin: 0;
-				position: relative;
-				display: block;
-			}
-			activity-graph table {
-				width: max-content;
-			}
-			activity-graph th,
-			activity-graph td {
-				text-align: left;
-			}
-
-			/* Theming */
-			:root {
-				--activity-graph-rounded: 2px;
-				--activity-graph-text-color: white;
-				--activity-graph-text-weight: 400;
-				--activity-graph-font-size: 12px;
-			}
-			:root,
-			activity-graph.dark {
-				--activity-graph-level-0-bg: #161b22;
-				--activity-graph-level-0-border: rgba(27, 31, 35, 0.06);
-				--activity-graph-level-1-bg: #0e4429;
-				--activity-graph-level-1-border: rgba(255, 255, 255, 0.05);
-				--activity-graph-level-2-bg: #006d32;
-				--activity-graph-level-2-border: rgba(255, 255, 255, 0.05);
-				--activity-graph-level-3-bg: #26a641;
-				--activity-graph-level-3-border: rgba(255, 255, 255, 0.05);
-				--activity-graph-level-4-bg: #39d353;
-				--activity-graph-level-4-border: rgba(255, 255, 255, 0.05);
-				--activity-graph-disabled-bg: transparent;
-			}
-			@media (prefers-color-scheme: light) {
-				:root {
-					--activity-graph-text-color: #24292e;
-					--activity-graph-level-0-bg: #ebedf0;
-					--activity-graph-level-0-border: rgba(27, 31, 35, 0.06);
-					--activity-graph-level-1-bg: #9be9a8;
-					--activity-graph-level-1-border: rgba(27, 31, 35, 0.06);
-					--activity-graph-level-2-bg: #40c463;
-					--activity-graph-level-2-border: rgba(27, 31, 35, 0.06);
-					--activity-graph-level-3-bg: #30a14e;
-					--activity-graph-level-3-border: rgba(27, 31, 35, 0.06);
-					--activity-graph-level-4-bg: #216e39;
-					--activity-graph-level-4-border: rgba(27, 31, 35, 0.06);
-					--activity-graph-disabled-bg: transparent;
-					--activity-graph-disabled-border: rgba(27, 31, 35, 0.06);
+		return html`
+			<style>
+				/* Global */
+				activity-graph {
+					color-scheme: light dark;
+					font-size: 12px;
+					display: block;
 				}
-			}
+				activity-graph figure {
+					overflow-x: auto;
+					margin: 0;
+					position: relative;
+					display: block;
+				}
+				activity-graph table {
+					width: max-content;
+				}
+				activity-graph th,
+				activity-graph td {
+					text-align: left;
+				}
 
-			/* Headings */
-			activity-graph th {
-				font-weight: var(--activity-graph-text-weight);
-				color: var(--activity-graph-text-color);
-				text-align: left;
-				position: relative;
-			}
-			activity-graph th.weekday {
-				width: 3em;
-				height: 1em;
-			}
-			activity-graph th.year,
-			activity-graph th.month {
-				height: 1.25em;
-			}
-			activity-graph th span {
-				clip-path: none;
-				position: absolute;
-				top: -0.2em;
-			}
-			activity-graph th.weekday span {
-				top: -0.2em;
-			}
-			activity-graph tr:nth-of-type(2n + 1) th.weekday span,
-			.sr-only {
-				clip: rect(0 0 0 0);
-				clip-path: inset(50%);
-				height: 1px;
-				overflow: hidden;
-				position: absolute;
-				left: 0;
-				top: 0;
-				white-space: nowrap;
-				width: 1px;
-			}
+				/* Theming */
+				:root {
+					--activity-graph-rounded: 2px;
+					--activity-graph-text-color: white;
+					--activity-graph-text-weight: 400;
+					--activity-graph-font-size: 12px;
+				}
+				:root,
+				activity-graph.dark {
+					--activity-graph-level-0-bg: #161b22;
+					--activity-graph-level-0-border: rgba(27, 31, 35, 0.06);
+					--activity-graph-level-1-bg: #0e4429;
+					--activity-graph-level-1-border: rgba(255, 255, 255, 0.05);
+					--activity-graph-level-2-bg: #006d32;
+					--activity-graph-level-2-border: rgba(255, 255, 255, 0.05);
+					--activity-graph-level-3-bg: #26a641;
+					--activity-graph-level-3-border: rgba(255, 255, 255, 0.05);
+					--activity-graph-level-4-bg: #39d353;
+					--activity-graph-level-4-border: rgba(255, 255, 255, 0.05);
+					--activity-graph-disabled-bg: transparent;
+				}
+				@media (prefers-color-scheme: light) {
+					:root {
+						--activity-graph-text-color: #24292e;
+						--activity-graph-level-0-bg: #ebedf0;
+						--activity-graph-level-0-border: rgba(27, 31, 35, 0.06);
+						--activity-graph-level-1-bg: #9be9a8;
+						--activity-graph-level-1-border: rgba(27, 31, 35, 0.06);
+						--activity-graph-level-2-bg: #40c463;
+						--activity-graph-level-2-border: rgba(27, 31, 35, 0.06);
+						--activity-graph-level-3-bg: #30a14e;
+						--activity-graph-level-3-border: rgba(27, 31, 35, 0.06);
+						--activity-graph-level-4-bg: #216e39;
+						--activity-graph-level-4-border: rgba(27, 31, 35, 0.06);
+						--activity-graph-disabled-bg: transparent;
+						--activity-graph-disabled-border: rgba(
+							27,
+							31,
+							35,
+							0.06
+						);
+					}
+				}
 
-			/* Cells */
-			activity-graph .day {
-				width: 1em;
-				height: 1em;
-				outline-offset: -1px;
-				border-radius: var(--activity-graph-rounded);
-			}
-			activity-graph .level-0 {
-				background-color: var(--activity-graph-level-0-bg);
-				outline: 1px solid var(--activity-graph-level-0-border);
-			}
-			activity-graph .level-1 {
-				background-color: var(--activity-graph-level-1-bg);
-				outline: 1px solid var(--activity-graph-level-1-border);
-			}
-			activity-graph .level-2 {
-				background-color: var(--activity-graph-level-2-bg);
-				outline: 1px solid var(--activity-graph-level-2-border);
-			}
-			activity-graph .level-3 {
-				background-color: var(--activity-graph-level-3-bg);
-				outline: 1px solid var(--activity-graph-level-3-border);
-			}
-			activity-graph .level-4 {
-				background-color: var(--activity-graph-level-4-bg);
-				outline: 1px solid var(--activity-graph-level-4-border);
-			}
-			activity-graph .disabled {
-				background-color: var(--activity-graph-disabled-bg);
-			}
-			/* Legend */
-			activity-graph figcaption {
-				display: flex;
-				align-items: center;
-				justify-content: center;
-				gap: 0.25em;
-				margin-top: -1.25em;
-				position: sticky;
-				left: 0;
-			}
-			activity-graph table {
-				margin-bottom: 2em;
-			}
-			activity-graph figcaption div:first-of-type {
-				margin-right: 0.5em;
-			}
-			activity-graph figcaption div:last-of-type {
-				margin-left: 0.5em;
-			}
+				/* Headings */
+				activity-graph th {
+					font-weight: var(--activity-graph-text-weight);
+					color: var(--activity-graph-text-color);
+					text-align: left;
+					position: relative;
+				}
+				activity-graph th.weekday {
+					width: 3em;
+					height: 1em;
+				}
+				activity-graph th.year,
+				activity-graph th.month {
+					height: 1.25em;
+				}
+				activity-graph th span {
+					clip-path: none;
+					position: absolute;
+					top: -0.2em;
+				}
+				activity-graph th.weekday span {
+					top: -0.2em;
+				}
+				activity-graph tr:nth-of-type(2n + 1) th.weekday span,
+				.sr-only {
+					clip: rect(0 0 0 0);
+					clip-path: inset(50%);
+					height: 1px;
+					overflow: hidden;
+					position: absolute;
+					left: 0;
+					top: 0;
+					white-space: nowrap;
+					width: 1px;
+				}
+
+				/* Cells */
+				activity-graph .day {
+					width: 1em;
+					height: 1em;
+					outline-offset: -1px;
+					border-radius: var(--activity-graph-rounded);
+				}
+				activity-graph .level-0 {
+					background-color: var(--activity-graph-level-0-bg);
+					outline: 1px solid var(--activity-graph-level-0-border);
+				}
+				activity-graph .level-1 {
+					background-color: var(--activity-graph-level-1-bg);
+					outline: 1px solid var(--activity-graph-level-1-border);
+				}
+				activity-graph .level-2 {
+					background-color: var(--activity-graph-level-2-bg);
+					outline: 1px solid var(--activity-graph-level-2-border);
+				}
+				activity-graph .level-3 {
+					background-color: var(--activity-graph-level-3-bg);
+					outline: 1px solid var(--activity-graph-level-3-border);
+				}
+				activity-graph .level-4 {
+					background-color: var(--activity-graph-level-4-bg);
+					outline: 1px solid var(--activity-graph-level-4-border);
+				}
+				activity-graph .disabled {
+					background-color: var(--activity-graph-disabled-bg);
+				}
+				/* Legend */
+				activity-graph figcaption {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					gap: 0.25em;
+					margin-top: -1.25em;
+					position: sticky;
+					left: 0;
+				}
+				activity-graph table {
+					margin-bottom: 2em;
+				}
+				activity-graph figcaption div:first-of-type {
+					margin-right: 0.5em;
+				}
+				activity-graph figcaption div:last-of-type {
+					margin-left: 0.5em;
+				}
+			</style>
 		`;
 	}
 
 	return html`${render()}`;
 }
+
+export default ActivityGraphElement;
